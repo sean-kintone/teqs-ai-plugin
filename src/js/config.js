c4b33096beea773e513adc260a77ecc159e35785
seanluse <sean@kintone.com>
2024-10-10 17:18:16 +0900

    api key in config screen

--- src/js/config.js
+++ src/js/config.js
@@ -1,20 +1,20 @@
 (function (PLUGIN_ID) {
   const formEl = document.querySelector('.js-submit-settings');
   const cancelButtonEl = document.querySelector('.js-cancel-button');
-  const messageEl = document.querySelector('.js-text-message');
-  if (!(formEl && cancelButtonEl && messageEl)) {
+  const keyEl = document.querySelector('.js-text-message');
+  if (!(formEl && cancelButtonEl && keyEl)) {
     throw new Error('Required elements do not exist.');
   }
 
   const config = kintone.plugin.app.getConfig(PLUGIN_ID);
-  if (config.message) {
-    messageEl.value = config.message;
+  if (config.key) {
+    keyEl.value = config.key;
   }
 
   formEl.addEventListener('submit', (e) => {
     e.preventDefault();
-    kintone.plugin.app.setConfig({ message: messageEl.value }, () => {
-      alert('The plug-in settings have been saved. Please update the app!');
+    kintone.plugin.app.setConfig({ key: keyEl.value }, () => {
+      alert('設定が保存されました。アプリを更新してください。');
       window.location.href = '../../flow?app=' + kintone.app.getId();
     });
   });
